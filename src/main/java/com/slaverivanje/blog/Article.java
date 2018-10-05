fdee7a83f85a49a407c956e019256f3b373f4250
javarivanje <javarivanje@gmail.com>
2018-10-05 18:29:01 +0200

    klasa Artical po dogovoru, bez konstruktora i get/setera

---
+++ src/main/java/com/slaverivanje/blog/Article.java
@@ -0,0 +1,17 @@
+package com.slaverivanje.blog;
+
+import java.util.Date;
+
+public class Article {
+    private String id;
+
+    private String title;
+
+    private String link;
+
+    private String summary;
+
+    private String body;
+
+    private Date createdDate = new Date();
+}
