f2ee4a4eb118a525f4042c974d6897eb25634d6e
javarivanje <javarivanje@gmail.com>
2018-10-05 18:35:26 +0200

    dodati kontruktor i get i set metode

--- src/main/java/com/slaverivanje/blog/Article.java
+++ src/main/java/com/slaverivanje/blog/Article.java
@@ -3,6 +3,7 @@ package com.slaverivanje.blog;
 import java.util.Date;
 
 public class Article {
+
     private String id;
 
     private String title;
@@ -14,4 +15,55 @@ public class Article {
     private String body;
 
     private Date createdDate = new Date();
+
+    public Article() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
 }
