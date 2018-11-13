32423237ac5cd39b73ebf316a20197f622fb55b6
javarivanje <javarivanje@gmail.com>
2018-11-13 22:03:59 +0100

    radi za title i url

--- src/main/java/com/slaverivanje/blog/service/QuizParsing.java
+++ src/main/java/com/slaverivanje/blog/service/QuizParsing.java
@@ -11,38 +11,28 @@ public class QuizParsing {
 
     public static void main(String[] args) {
 
-        /*Pristup source file-u i pravljenje stream-a,
-        koji ce da posluzi kao parametar konstruktora scanner klase
-         */
         ClassPathResource quizResource = new ClassPathResource("/static/quiz/unicorns.adoc");
         try(InputStream stream = quizResource.getInputStream();
             Scanner sc = new Scanner(stream)){
             Quiz quiz = new Quiz();
             String str;
-            while(sc.hasNextLine()) //dokle god postoji nova linija teksta proveravati dole navedene "if" uslove
-            {
-                if(sc.findInLine(":title:").equals(":title:"))//ako nadje match u liniji tj. ako je naisao na "title" liniju
-                {
-                   //postaviti vrednost quiz.title koju cemo dobiti kada prosledimo ostatak linije posle ":title:" kojoj skidamo navodnike
+            while(sc.hasNextLine()){
+                str = sc.next();
+                if (str!=null && str.equals(":title:")){
                    str = sc.nextLine();
-                   quiz.setTitle(str.substring(1,str.length()-1));
+                   quiz.setTitle(str.substring(2,str.length()-1));
+                   continue;
                 }
-
-                if(sc.findInLine(":url:").equals(":url:"))//ponavljamo isto, ovoga puta za ":url:"
-                {
+                if(str!=null && str.equals(":url:")){
                     str = sc.nextLine();
-                    quiz.setTitle(str.substring(1,str.length()-1));
+                    quiz.setUrl(str.substring(2,str.length()-1));
+                    continue;
                 }
-
+                sc.nextLine();
             }
-
-            System.out.println("Posle petlje while");
+            System.out.println(quiz);
         } catch (IOException ioe) {
             throw new RuntimeException(ioe.getMessage(), ioe);
         }
-
-
     }
-
-
 }
